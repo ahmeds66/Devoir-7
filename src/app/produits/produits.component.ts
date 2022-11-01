a75ebaa1c1142e5672df68e0816e952aacd7fc8b
Ahmed Souissi <souissiahmed66@gmail.com>
2022-11-01 22:44:07 +0100

    Add files via upload

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+import { ActivatedRoute,Router } from '@angular/router';
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html'
+})
+export class ProduitsComponent implements OnInit {
+
+    produits? : Produit[]; //un tableau de produits
+
+  constructor(private produitService: ProduitService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute ) { 
+   //this.produits=[];
+     }
+
+  ngOnInit(): void {
+  //this.produits = this.produitService.listeProduits();
+  this.produitService.listeProduit().subscribe(prods => {
+    console.log(prods);
+    this.produits = prods;
+  });
+  }
+  /*supprimerProduit(p: Produit) {
+  //console.log(p);
+    let conf = confirm("Etes-vous sûr ?");
+    if (conf) 
+     this.produitService.supprimerProduit(p); 
+  }
+  */
+  supprimerProduit(prod: Produit) { 
+    let conf = confirm("Etes-vous sûr ?"); 
+    if (conf)
+      this.produits?.forEach((cur, index) => {
+        if(prod.idProduit === cur.idProduit) {
+          this.produits?.splice(index,1);
+        }
+      });
+  }
+    /*this.produitService.supprimerProduit(prod.idProduit).subscribe(() => { 
+      console.log("produit supprimé"); 
+    }); 
+    this.router.navigate(['produits']).then(() => { 
+      window.location.reload(); 
+    });
+    */ 
+  
+ 
+
+}
